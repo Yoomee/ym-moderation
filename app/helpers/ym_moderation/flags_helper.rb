8cd31c0ad610ccb73a9228228e6136efdf7d9732
Edward Andrews-Hodgson <edward@yoomee.com>
2013-10-15 16:28:17 +0100

    Don't set class of flag_link

--- app/helpers/ym_moderation/flags_helper.rb
+++ app/helpers/ym_moderation/flags_helper.rb
@@ -4,7 +4,7 @@ module YmModeration::FlagsHelper
     options = args.extract_options!
     link_text, resource = args.length > 1 ? [args[0], args[1]] : ['Report', args[0]]
     url = new_flagging_path(:resource_type => resource.class.to_s, :resource_id => resource.id)
-    link_to(link_text, url, options.merge(:remote => true, :class => 'flag-link'))
+    link_to(link_text, url, options.merge(:remote => true))
   end
 
 end
