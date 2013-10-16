7cf56b1187e2c9efddeef9c4fc6bfe25cfd87a10
Edward Andrews <edward@aldreth.com>
2013-10-16 11:59:04 +0100

    Add abilities to install_generator

--- lib/generators/ym_moderation/install_generator.rb
+++ lib/generators/ym_moderation/install_generator.rb
@@ -10,9 +10,16 @@ module YmModeration
       def manifest
         # examples ->
         # copy_file "models/page.rb", "app/models/page.rb"
-        # if should_add_abilities?('Page')
-        #   add_ability(:open, "can :show, Page, :draft => false")
-        # end
+        if should_add_abilities?('Flag')
+          add_ability(:admin, "can :manage, Flag")
+        end
+        if should_add_abilities?('Flagging')
+          add_ability(:open, "can :create, Flagging")
+          add_ability(:admin, "can :manage, Flagging")
+        end
+        if should_add_abilities?('FlaggingCategory')
+          add_ability(:admin, "can :manage, FlaggingCategory")
+        end
         # try_migration_template "migrations/create_pages.rb", "db/migrate/create_pages"
 
         copy_file "controllers/flaggings_controller.rb", "app/controllers/flaggings_controller.rb"
