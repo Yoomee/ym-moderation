2177a9847e43e56bcd7fc0a218ec88ca430b5f79
Edward Andrews <edward@aldreth.com>
2013-10-16 11:43:16 +0100

    Add logged in tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,11 @@ FactoryGirl.define do
     text Faker::Lorem.paragraphs(2)
   end
 
+  factory :user do
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    email {|u| Faker::Internet.email("#{u.first_name} #{u.last_name}")}
+    password "password"
+  end
+
 end
--- spec/integration/flagging_a_post_spec.rb
+++ spec/integration/flagging_a_post_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe 'Flagging a post', :js => true do
 
+  let(:post) {FactoryGirl.create(:post)}
+  
   context 'logged out' do
-
-    let(:post) {FactoryGirl.create(:post)}
 
     before do
       visit "/posts/#{post.id}"
@@ -38,8 +38,58 @@ describe 'Flagging a post', :js => true do
           modal_body = find(:css, "#Post#{post.id} .modal-body")
           expect(modal_body).to have_content('Thanks for telling us!')
           expect(Flag.where(:resource_type => 'Post', :resource_id => post.id)).to exist
-          flag = Flag.where(:resource_type => 'Post', :resource_id => post.id).first          
+          flag = Flag.where(:resource_type => 'Post', :resource_id => post.id).first
           expect(flag.flaggings.where(:description => 'This is inappropriate', :email => 'edward@yoomee.com')).to exist
+        end
+
+      end
+
+    end
+
+  end
+
+  context 'logged in' do
+    
+    let(:user) {FactoryGirl.create(:user)}
+
+    before do
+      visit '/login'
+      fill_in('user_email', :with => user.email)
+      fill_in('user_password', :with => 'password')
+      click_on 'Login'
+      visit "/posts/#{post.id}"
+    end
+
+    it 'displays a flag link' do
+      expect(page).to have_xpath("//a[@href = '/flaggings/new?resource_id=#{post.id}&resource_type=Post']")
+    end
+
+    context 'clicked flag link' do
+
+      before do
+        click_link('Report')
+      end
+
+      it 'displays modal with flags#form' do
+        expect(page).to have_css("#Post#{post.id}")
+      end
+
+      context 'submitted flags#form' do
+
+        before do
+          within("#Post#{post.id}") do
+            fill_in 'flagging_description', :with => 'This is inappropriate'
+            click_on 'Report content'
+          end
+        end
+
+        it 'displays thanks message and create flag' do
+          modal_body = find(:css, "#Post#{post.id} .modal-body")
+          expect(modal_body).to have_content('Thanks for telling us!')
+          expect(Flag.where(:resource_type => 'Post', :resource_id => post.id)).to exist
+          flag = Flag.where(:resource_type => 'Post', :resource_id => post.id).first
+          puts flag.flaggings.first.inspect
+          expect(flag.flaggings.where(:description => 'This is inappropriate', :user_id => user.id)).to exist
         end
 
       end
